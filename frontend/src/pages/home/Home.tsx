c86b8497ec9463649b6941b0cca75f12d77146d0
Kay Zin <kay_zin_soe@mpa.gov.sg>
2026-09-01 18:34:40 +0800

    chore: remove obsolete files

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -24,8 +24,6 @@ export default function Home({ onNavigate }: { onNavigate?: (to: string) => void
       {/* <CatchBallGame />          */}
       <VesselList />
 
-
-
      
     </MainLayout>
   )
